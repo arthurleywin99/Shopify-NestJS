1190893a18e222fd422b11bcd16328d5e3b8c8f3
Giang Bui <buinguyentruonggiang.1112@gmail.com>
2025-05-23 15:38:35 +0700

    [Feature]: Config app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from './authentication/authentication.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule, CommonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
